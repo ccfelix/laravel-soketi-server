1becfe956d23fcb65bdaa2e5faa23ed307e92755
Clebio Felix <clebiofelix@outlook.com>
2022-01-18 15:03:18 -0300

    feat: implementa realtime websocket com Soketi -> soketi.app

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,6 +41,20 @@ return [
             ],
         ],
 
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY', 'app-key'),
+            'secret' => env('PUSHER_APP_SECRET', 'app-secret'),
+            'app_id' => env('PUSHER_APP_ID', 'app-id'),
+            'options' => [
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'encrypted' => true,
+                'useTLS' => env('PUSHER_SCHEME') === 'https',
+            ],
+        ],
+
         'ably' => [
             'driver' => 'ably',
             'key' => env('ABLY_KEY'),
